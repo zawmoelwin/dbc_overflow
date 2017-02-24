ceaf396c850f787ee516b459e898d030cf32f223
Zaw Moe Lwin <email": null>
2017-02-24 11:58:46 -0800

    update the association between the questions and the answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :answerer , class_name: :User
+  belongs_to :questions
   has_many :comments, as: :commentable
   has_many :votes , as: :votable
 
